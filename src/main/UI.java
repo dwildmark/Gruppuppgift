bdb368aa3160aa9fdb7b24ce5ca246499f4fa07b
dwildmark <d.wildmark@gmail.com>
2015-03-10 03:05:26 +0100

    glömde spara...

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,8 +1,13 @@
 package main;
 
-public class UI {
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+public class UI extends JPanel{
 	private GUController controller;
 	private MapView mapView;
+	private JPanel karta, text, inputPnl;
+	private JComboBox<Place> dropDownList;
 	
 	public UI(GUController controller, MapView mapView) {
 		this.controller = controller;

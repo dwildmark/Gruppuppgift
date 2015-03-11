59a416e4d2ad152dd52c060b7572bf2b0709be5d
AndreasLanghammer <andreaslangahmmer47@gmail.com>
2015-03-11 16:03:21 +0100

    BST update
    Har kommit vidare i BST klassen, dock skapar vi ingen träd än, lite
    fundersam på hur jag ska kode detta, men de löser sig. lagt till ett
    paket med klasser från föreläsningar som är användbart till uppgiften.

--- src/main/BST.java
+++ src/main/BST.java
@@ -1,18 +1,68 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.util.ArrayList;
 
 public class BST {
+	private Node tree;
 	
-	public String getInfo (String city) throws FileNotFoundException{
-		BufferedReader br = new BufferedReader(new FileReader("places.txt"));
-		
-		
-		
-		return city;
-		
+	/**
+	 * Metoden ska lägga till nya platser tills det inte finns fler platser att 
+	 * lägga till. 
+	 * 
+	 * OBS!! metod för att "sortera" in places i det binära sökträdet
+	 * måste skapas, typ metod: expandTree eller liknande
+	 * @param places, olika städer.
+	 */
+
+	public BST(ArrayList<Place> places){
+		for(int i = 0; i < places.size(); i++){
+			
+
+		}
+	}
+
+	public Place get(String key) {
+		Node node = find( key );
+		if(node!=null)
+			return node.value;
+		return null;
+	}
+	/**
+	 * Om tree är större än key så ska jag söka åt vänster. Annars ska jag söka åt höger.
+	 * När while-satsen blir 0 så har vi hittat noden,
+	 * när res > 0 går vi åt vänster och när res < 0, åt höger.
+	 * @param key staden vi söker efter
+	 * @return funnen stad.
+	 */
+	private Node find(String key) {
+		int res;
+		Node node = tree;
+		while( ( node != null ) && ( ( res = tree.key.compareTo( key ) ) != 0 ) ) {
+			if( res > 0 )
+				node = node.left;
+			else
+				node = node.right;
+		}
+		return node;
+	}
+
+	private class Node {
+		private String key;
+		private Place value;
+		private Node left;
+		private Node right;
+
+		public Node (String key, Place value, Node left, Node right){
+			this.key = key;
+			this.value = value;
+			this.left = left;
+			this.right = right;
+		}
+
+		public int compareTo(Node nodeNew){
+			return key.compareTo(nodeNew.key);
+		}
+
 	}
 
 }
